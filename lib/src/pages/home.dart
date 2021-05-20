cc886dd8cf1c44de3ad12bd8ab38fecd9233c0e9
Uriel Aispuro <urielalexis64@gmail.com>
2021-05-19 20:09:33 -0600

    home page, alert dialog

--- lib/src/pages/home.dart
+++ lib/src/pages/home.dart
@@ -1,8 +1,92 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+import 'package:band_names_app/src/models/band.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage>
+    with SingleTickerProviderStateMixin {
+  List<Band> bands = [
+    Band(id: '1', name: 'Metallica', votes: 99),
+    Band(id: '2', name: 'Megadeth', votes: 66),
+    Band(id: '3', name: 'Slayer', votes: 11),
+    Band(id: '4', name: 'AC/DC', votes: 23),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 1,
+        title: Text(
+          'Band Names',
+          style: TextStyle(color: Colors.black87),
+        ),
+        backgroundColor: Colors.white,
+      ),
+      body: ListView.builder(
+        itemCount: bands.length,
+        itemBuilder: (context, index) => _bandTile(bands[index]),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: addNewBand,
+        child: Icon(Icons.add),
+        elevation: 1,
+      ),
+    );
+  }
+
+  ListTile _bandTile(Band band) {
+    return ListTile(
+      leading: CircleAvatar(
+        child: Text('${band.name.substring(0, 2)}'),
+        backgroundColor: Colors.blue[100],
+      ),
+      title: Text(band.name),
+      trailing: Text(
+        '${band.votes}',
+        style: TextStyle(fontSize: 20),
+      ),
+      onTap: () {},
+    );
+  }
+
+  addNewBand() {
+    final textController = new TextEditingController();
+
+    return showDialog(
+        context: context,
+        builder: (_) => AlertDialog(
+              title: Text('Band Name:'),
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(20))),
+              content: TextField(
+                controller: textController,
+              ),
+              actions: <Widget>[
+                MaterialButton(
+                    child: Text('Cancel'),
+                    elevation: 5,
+                    textColor: Colors.redAccent,
+                    onPressed: () => addBandToList(textController.text)),
+                MaterialButton(
+                    child: Text('Add'),
+                    elevation: 5,
+                    textColor: Colors.blueAccent,
+                    onPressed: () => addBandToList(textController.text))
+              ],
+            ));
+  }
+
+  void addBandToList(String name) {
+    if (name.trim().length != 0) {
+      setState(() {});
+      this.bands.add(Band(id: DateTime.now().toString(), name: name, votes: 0));
+    }
+
+    Navigator.pop(context);
   }
 }
