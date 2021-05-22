8ac02713a33cb0804d33c080d262aefebee084d0
Uriel Aispuro <urielalexis64@gmail.com>
2021-05-21 20:51:16 -0600

    environment variables

--- band_names_server/index.js
+++ band_names_server/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const path = require("path");
+require("dotenv").config();
 
 const app = express();
 
@@ -8,7 +9,7 @@ const publicPath = path.resolve(__dirname, "public");
 
 app.use(express.static(publicPath));
 
-app.listen(3000, (err) => {
+app.listen(process.env.PORT, (err) => {
 	if (err) throw new Error(err);
 
 	console.log(`Listening on port: `, 3000);
