b16fcd4f2588de38a683a95cee56fed5693e22c5
Uriel Aispuro <urielalexis64@gmail.com>
2021-05-23 16:18:46 -0600

    server ready

--- band_names_server/index.js
+++ band_names_server/index.js
@@ -2,14 +2,20 @@ const express = require("express");
 const path = require("path");
 require("dotenv").config();
 
+// Express App
 const app = express();
+
+// Node Server
+const server = require("http").createServer(app);
+module.exports.io = require("socket.io")(server);
+require("./sockets/socket");
 
 // Public path
 const publicPath = path.resolve(__dirname, "public");
 
 app.use(express.static(publicPath));
 
-app.listen(process.env.PORT, (err) => {
+server.listen(process.env.PORT, (err) => {
 	if (err) throw new Error(err);
 
 	console.log(`Listening on port: `, 3000);
